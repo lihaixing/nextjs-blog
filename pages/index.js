a71f5277b3c2431a32ee8107bc46a9bcf1d301b3
李海兴 <lihaixing@xylink.com>
2021-07-31 23:23:18 +0800

    fix: link

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 // @ts-nocheck
 import Head from 'next/head'
-import Link from 'next/Link'
+import Link from 'next/link'
 import Layout, { siteTitle } from '../components/layout'
 import utilStyles from '../styles/utils.module.css'
 import { getSortedPostsData } from '../lib/posts'
