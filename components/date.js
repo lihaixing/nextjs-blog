91afa6887a79016c11c94d70c905ffee59d58e0b
李海兴 <lihaixing@xylink.com>
2021-07-31 22:41:39 +0800

    fix: my blog

---
+++ components/date.js
@@ -0,0 +1,7 @@
+// @ts-nocheck
+import { parseISO, format } from 'date-fns'
+
+export default function Date({ dateString }) {
+  const date = parseISO(dateString)
+  return <time dateTime={dateString}>{format(date, 'LLLL d, yyyy')}</time>
+}
